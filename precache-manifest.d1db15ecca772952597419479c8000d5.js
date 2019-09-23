b1b0af55778203aeba763aa04ea1ac7f043397f2
Adam Florczak <adam.florczak@me.com>
2019-09-23 18:24:20 +0200

    Updates

---
+++ precache-manifest.d1db15ecca772952597419479c8000d5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5cad9422726085a13f4819e64db3d2",
+    "url": "/gpx-image-geotag/index.html"
+  },
+  {
+    "revision": "f0e1e4f8cb9b346a0102",
+    "url": "/gpx-image-geotag/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "0e549dddbd8af86b7507",
+    "url": "/gpx-image-geotag/static/css/main.27c1d2de.chunk.css"
+  },
+  {
+    "revision": "f0e1e4f8cb9b346a0102",
+    "url": "/gpx-image-geotag/static/js/2.ed15dd9a.chunk.js"
+  },
+  {
+    "revision": "0e549dddbd8af86b7507",
+    "url": "/gpx-image-geotag/static/js/main.af1f45c2.chunk.js"
+  },
+  {
+    "revision": "db7ae046312b0ebf1e9b",
+    "url": "/gpx-image-geotag/static/js/runtime-main.56c0669e.js"
+  },
+  {
+    "revision": "c5c4dbb63309b6823486d7aaac296ccd",
+    "url": "/gpx-image-geotag/static/media/icon.c5c4dbb6.svg"
+  },
+  {
+    "revision": "cccb897485813c7c256901dbca54ecf2",
+    "url": "/gpx-image-geotag/static/media/lato-bold.cccb8974.woff2"
+  },
+  {
+    "revision": "d878b6c29b10beca227e9eef4246111b",
+    "url": "/gpx-image-geotag/static/media/lato-bold.d878b6c2.woff"
+  },
+  {
+    "revision": "27bd77b9162d388cb8d4c4217c7c5e2a",
+    "url": "/gpx-image-geotag/static/media/lato-regular.27bd77b9.woff"
+  },
+  {
+    "revision": "bd03a2cc277bbbc338d464e679fe9942",
+    "url": "/gpx-image-geotag/static/media/lato-regular.bd03a2cc.woff2"
+  }
+]);
--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import { App } from './app.component';
-
-ReactDOM.render(<App />, document.getElementById('root'));
